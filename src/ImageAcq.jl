c82c63426d160c00a2066bdfc7e06137dbdc0570
arurz <arurz@inaoep.mx>
2021-05-04 23:51:09 -0500

    Documented scripts
    * All the scripts have their functions documented

--- src/ImageAcq.jl
+++ src/ImageAcq.jl
@@ -1,12 +1,15 @@
-using VisitingICF2021
-using Images
+"This script is intended to manipulate the multidimensional arrays from the image manipulation"
+
+import Images
 
 export imgload, chsplit
 
+"`imgload("path_string")` returns the array of values of a monochromatic or polychromatic image. In the first case is onlye an N×N matrix, in the second case is a collection of three N×N matrices."
 function imgload(path::String)
-    return load(path)
+    return Images.load(path)
 end
 
+"`chsplit(img, c)` returns the c-channel of and image _img_. It requires a polychromatic imported image from the `imgload` function, and a channel c ∈ {1, 2, 3} whose equivalences are 1 = r (red), 2 = g (green), 3 = b (blue)."
 function chsplit(img, c)
     chs = Images.channelview(img)
     if c == 1
@@ -16,4 +19,9 @@ function chsplit(img, c)
     elseif c == 3
         return chs[3,:,:]
     end
+end
+
+"`imgrep(r, g, b)` takes the channel arrays (original or transformated), and returns a stacked representation of dimensions N×N×3, suitable for plotting in the `imshow` command of PyPlot"
+function imgrep(r,g,b)
+    return permutedims(Images.StackedView(r, g, b), [3, 2, 1])
 end
--- src/ImgRots.jl
+++ src/ImgRots.jl
@@ -1,6 +1,10 @@
-using VisitingICF2021
-using LinearAlgebra
+"This script contains the implementation algorithms for the unitary transformation on discrete and finite fields. It acts as a single transformation, but it can be adecuate to be extended on a FOR loop to be applied on polychromatic fields
 
+It provides three functions: `fnm(n, m, j, imgdata)` maps the field from position to modes. `fnmθ(θ, n, m, j, imgdat)` adds an angle dependent phase. `Fqθ(θ, q₁, q₂, j, imgdat)` returns the transformed field in the modes to the position representation. This last function is the upper layer of the transformation, it can (could, should) be used to the polychromatic duties."
+
+using VisitingICF2021 # Call the parent module
+
+"`fnm(n, m, j, imgdat)` encodes a two-dimensional position field in a modes space labeled by mode _n_ and counter _m_, who ranges are n ∈ [0, 2j] and |m|₂ ≤ n. 'imgdat' is a position field, that is a N×N matrix, for N = 2j."
 function fnm(n, m, j, imgdat)
     vnm = zeros(ComplexF64, (2 * j + 1, 2 * j + 1))
     for q₁ in -j:j
@@ -11,12 +15,14 @@ function fnm(n, m, j, imgdat)
     return sum(vnm)
 end
 
+"`fnmθ(θ, n, m, j, imgdat)` is a function who applies an angle-dependent phase to the `fnm` function. It is the preamble to perform the transformation, given the value of θ."
 fnmθ(θ, n, m, j, imgdat) = exp(-1im * m * θ) * fnm(n, m, j, imgdat)
 
+"`Fqθ(θ, q₁, q₂, j, imgdat)` is the top function who perform the whole transformation on the position field. Given and N×N field, an angle θ, the values of the transformed pair (q₁, q₂) are given by this function. Note: It is extremely slow, so slow."
 function Fqθ(θ, q₁, q₂, j, imgdat)
     rd = 0.0
     ru = 0.0
     rd = sum([conj(LK(q₁, q₂, n, m, j)) * fnmθ(θ, n, m, j, imgdat) for n in 0:2*j for m in -n:2:n])
-    ru = sum([conj(LK(q₁, q₂, n, m, j)) * fnmθ(θ, n, m, j, imgdat) for n in 2*j:4*j for m in -(4 * j - n):2:(4 * j - n)])
+    ru = sum([conj(LK(q₁, q₂, n, m, j)) * fnmθ(θ, n, m, j, imgdat) for n in (2*j + 1):4*j for m in -(4 * j - n):2:(4 * j - n)])
     return rd + ru
 end
